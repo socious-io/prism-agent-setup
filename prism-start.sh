644999084304759a57e391a037d5df30684f0057
jeyem <me@e-mahmoudi.me>
2024-03-19 12:32:21 +0330

    fix commands

--- prism-start.sh
+++ prism-start.sh
@@ -1,21 +1,22 @@
+#!/usr/bin/env bash
+
 POSITIONAL_ARGS=()
 
 while [[ $# -gt 0 ]]; do
-	case $1 in
-	-t | --vault_token)
-		VAULT_TOKEN="$2"
-		shift # past argument
-		shift # past value
-		;;
-    *)
-	POSITIONAL_ARGS+=("$1") # save positional arg
-		shift                   # past argument
-		;;    
-	esac
+    case $1 in
+        -t|--vault_token)
+            VAULT_TOKEN="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift                    # past argument
+            ;;
+    esac
 done
 
-VAULT_TOKEN="${VAULT_TOKEN}"
+echo "VAULT_TOKEN = ${VAULT_TOKEN}"
 
-echo "NAME                   = ${VAULT_TOKEN}"
 
-VAULT_TOKEN=${VAULT_TOKEN} docker-compose -p prism -f docker-compose-prism.yml --env-file .env up -d 
+VAULT_TOKEN="${VAULT_TOKEN}" docker-compose -p prism -f docker-compose-prism.yml --env-file .env up -d
--- prism-stop.sh
+++ prism-stop.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 docker-compose -p prism -f docker-compose-prism.yml --env-file .env down -v
